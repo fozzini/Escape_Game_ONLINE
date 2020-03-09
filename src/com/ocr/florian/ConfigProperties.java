a2f4c0bfc3779e7388a640564809acbb7a6ab492
fozzini <ffoezon@gmail.com>
2020-03-09 08:55:02 +0100

    Amélioration de la gestion des exceptions dans le fichier configProperties.
    Signed-off-by: Florian Foezon <ffoezon@gmail.com>

--- src/com/ocr/florian/ConfigProperties.java
+++ src/com/ocr/florian/ConfigProperties.java
@@ -31,38 +31,39 @@ public class ConfigProperties {
         File configFile;
         configFile = new File("config.properties");
         try {
-        fis = new FileInputStream(configFile);
+            fis = new FileInputStream(configFile);
         } catch (FileNotFoundException e) {
-        logger.error("Fichier de configuration manquant, chargement par défaut");
-        return;
+            logger.error("Fichier de configuration manquant, utilisation par défaut");
+            return;
         }
         Properties propConfig = new Properties();
         try {
             propConfig.load(fis);
             String combiLength = propConfig.getProperty("combinationLength");
-            String mxTries = propConfig.getProperty("maxTries");
-            String devMode = propConfig.getProperty("developerMode");
-            devMode = devMode.toLowerCase();
-
-            logger.error("Fichier de configuration manquant, chargement par défaut");
             if (Integer.parseInt(combiLength) > 0) {
                 combinationLength = Integer.parseInt(combiLength);
                 if (combinationLength <= 2) {
-                    logger.warn("Combinaison inférieur ou égal à 2 chiffres.");
+                    logger.warn("Combinaison inférieure ou égale à 2 chiffres.");
                 }
             }
+        } catch (NumberFormatException e) {
+            logger.error("Erreur du fichier de configuration, chargement par défaut");
+        }
+        try {
+            String mxTries = propConfig.getProperty("maxTries");
             if (Integer.parseInt(mxTries) > 0) {
                 maxTries = Integer.parseInt(mxTries);
                 if (maxTries <= 2) {
                     logger.warn("Nombre d'essais inférieur ou égal à 2.");
                 }
             }
-            if (devMode.contains("true") || devMode.contains("false")) {
-                developerMode = Boolean.parseBoolean(devMode);
-            }
-
         } catch (NumberFormatException e) {
             logger.error("Erreur du fichier de configuration, chargement par défaut");
+        }
+        String devMode = propConfig.getProperty("developerMode");
+        devMode = devMode.toLowerCase();
+        if (devMode.contains("true") || devMode.contains("false")) {
+            developerMode = Boolean.parseBoolean(devMode);
         }
     }
 }
